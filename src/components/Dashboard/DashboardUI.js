e6e38cd2ef8bc78288332db510c099224cd6d98d
andrewwachira <andrewwachira1@gmail.com>
2024-09-29 23:59:36 +0300

    build error fix: mainly added activity log functionality and improved dashboard, with other small small improvements

--- src/components/Dashboard/DashboardUI.js
+++ src/components/Dashboard/DashboardUI.js
@@ -3,7 +3,6 @@ import dynamic from "next/dynamic";
 import {useState} from "react";
 // import ChartOne from "../Charts/ChartOne";
 // import ChartTwo from "../Charts/ChartTwo";
-import ChatCard from "../Chat/ChatCard";
 import TableOne from "../Tables/TableOne";
 import CardDataStats from "../CardDataStats";
 import useColorMode from "@/hooks/useColorMode";
