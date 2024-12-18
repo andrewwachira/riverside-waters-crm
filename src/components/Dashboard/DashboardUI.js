80abe7f69e2af3f7323898d08555b4d94f654e9d
andrewwachira <andrewwachira1@gmail.com>
2024-12-18 09:22:17 +0300

    removed useswr on dashboard, replaced with useEffect fetch

--- src/components/Dashboard/DashboardUI.js
+++ src/components/Dashboard/DashboardUI.js
@@ -1,6 +1,6 @@
 "use client"
 import dynamic from "next/dynamic";
-import {useState} from "react";
+import {useState,useEffect} from "react";
 // import ChartOne from "../Charts/ChartOne";
 // import ChartTwo from "../Charts/ChartTwo";
 import TableOne from "../Tables/TableOne";
@@ -10,6 +10,7 @@ import useSWR from "swr";
 import Image from "next/image";
 import Link from "next/link";
 import avatar from "../../../public/images/avatar.png";
+import axios from "axios";
 // const ChartThree = dynamic(() => import("@/components/Charts/ChartThree"), {
 //   ssr: false,
 // });
@@ -19,39 +20,42 @@ const   DashboardUI = () => {
   const [loading,setLoading] = useState(false);
   const [error,setError] = useState(false);
   const [dashData,setDashData] = useState(null);
-  const dashFetcher = async(key) => {
-    setLoading(true);
-    const res = await fetch(key);
-    const data = await res.json();
-    if(res.status !== 200){
-      setLoading(false);
-      setError(res?.error?.message);
-    }else{
-      setLoading(false);
-      setDashData(data.payload);
+  useEffect(()=>{
+    async function getDashData (){
+      try {
+        setLoading(true);
+        const {data} = await axios.get("/api/dashboard");
+        setLoading(false);
+        setDashData(data.payload);
+      } catch (error) {
+        setError(error.message);
+        toast.error(error.message)
+      }
     }
-  }
-  const {isValidating} = useSWR("/api/dashboard",dashFetcher,{revalidateIfStale: true,revalidateOnReconnect:true,revalidateOnFocus:true});
+    getDashData();
+  },[]);
+
   return (
     <>
+      {error && <div className="p-3 bg-rose-300 rounded-md mb-2 border border-rose-600 text-rose-600">{error}</div>}
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
-        <CardDataStats title="Upcoming Filter Changes" total={isValidating ? <div className="cssLoader"></div> : dashData?.numUpcomingFilters }>
+        <CardDataStats title="Upcoming Filter Changes" total={loading ? <div className="cssLoader"></div> : dashData?.numUpcomingFilters }>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 21 3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5" />
         </svg>
         </CardDataStats>
-        <CardDataStats title="System Admin(s)" total={isValidating ? <div className="cssLoader"></div> : dashData?.numAdmins } >
+        <CardDataStats title="System Admin(s)" total={loading ? <div className="cssLoader"></div> : dashData?.numAdmins } >
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
         </svg>
         </CardDataStats>
     
-        <CardDataStats title="Total Clients" total={isValidating ? <div className="cssLoader"></div> : dashData?.numClients }>
+        <CardDataStats title="Total Clients" total={loading ? <div className="cssLoader"></div> : dashData?.numClients }>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
         </svg>
         </CardDataStats>
-        <CardDataStats title="Filters Due" total={isValidating ? <div className="cssLoader"></div> : dashData?.numFiltersDue }>
+        <CardDataStats title="Filters Due" total={loading ? <div className="cssLoader"></div> : dashData?.numFiltersDue }>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="red" className="size-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
         </svg>
@@ -67,7 +71,7 @@ const   DashboardUI = () => {
         <div className="col-span-12 xl:col-span-12">
           <div className="rounded-sm border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
             <h4 className="mb-6 text-xl font-semibold text-black dark:text-white">
-              Upcoming Filter changes {isValidating && <div className="cssLoader"></div> }
+              Upcoming Filter changes {loading && <div className="cssLoader"></div> }
             </h4>
           { dashData?.upcomingFilters.length < 1 ?
                 <p className="mb-3">There are no upcoming filter events</p>
@@ -141,7 +145,7 @@ const   DashboardUI = () => {
         <div className="col-span-12 xl:col-span-12">
           <div className="rounded-sm border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
             <h4 className="mb-6 text-xl font-semibold text-black dark:text-white">
-              Filter changes Due {isValidating && <div className="cssLoader"></div> }
+              Filter changes Due {loading && <div className="cssLoader"></div> }
             </h4>
           { dashData?.filtersDue.length < 1 ?
                 <p className="mb-3">There are no filter change events due</p>
@@ -214,7 +218,7 @@ const   DashboardUI = () => {
 
         <div className="col-span-12 rounded-sm border border-stroke bg-white py-6 shadow-default dark:border-strokedark dark:bg-boxdark xl:col-span-4">
           <h4 className="mb-6 px-7.5 text-xl font-semibold text-black dark:text-white">
-            Admins {isValidating && <div className="cssLoader"></div> }
+            Admins {loading && <div className="cssLoader"></div> }
           </h4>
 
           <div>
